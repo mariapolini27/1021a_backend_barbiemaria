85ca868780031aac50ca168ea41247ace71cfe72
mariapolini27 <maria.polini@estudante.ifms.edu.br>
2023-11-24 10:07:38 -0400

    atividade final

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import express, {Request} from 'express';
 import BancoMongoDB from './infra/banco/banco-mongodb';
-import ListarFilme from './aplicacao/listar-filme.use-case'
-import SalvarFilme from './aplicacao/salva-filme.use-case'
-const bancoMongoDB = new BancoMongoDB()
+import ListarFilme from './aplicacao/listar-filme.use-case';
+import SalvarFilme from './aplicacao/salva-filme.use-case';
+import cors from 'cors';
+
+const bancoMongoDB = new BancoMongoDB();
+
+
 const app = express();
 app.use(express.json())
+
+app.use(cors())
 
 
 app.get('/filmes', async (req, res) => {
@@ -23,25 +29,32 @@ app.post('/filmes', async (req:Request, res) => {
     }
     const salvarFilme = new SalvarFilme(bancoMongoDB)
     const filmes = await salvarFilme.execute(filme)
-    const cadastrorepetido = filmes_repositorio.find(filme => filme.id === id)
-    if(cadastrorepetido){
-        return res.status(400).send({error: 'Filme já cadastrado.'});
+   
+    const filmerepetido = filmes_repositorio.find(filme => filme.id === id)
+    if(filmerepetido){
+         return res.status(400).send({error: 'Filme já cadastrado'})
     }
+
     filmes_repositorio.push(filme)
     res.status(201).send(filmes)
 });
 
 app.delete('/filmes/:id', (req, res) => {
     const id = parseInt(req.params.id)
+
     const filme = filmes_repositorio.find(filme => filme.id === id)
+   
     if (!filme) return res.status(404).send(filme)
+
     const filterFilme = filmes_repositorio.filter(filme => filme.id !== id)
+   
     filmes_repositorio = filterFilme
+   
     res.status(200).send(filme)
 });
 
 
-// Inicia o servidor
+
 app.listen(3000, () => {
     console.log('Servidor iniciado na porta 3000');
 });
